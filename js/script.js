ab7af84d56ee70341b506ca7826e72258fdbcc90
kimkwanka <kimkwanka@gmail.com>
2021-04-29 23:43:21 +0200

    feat: add pokemonList

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-const favoriteFood = 'Pizza';
-alert('Hello World');
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList[0] = {
+  name: 'Bulbasaur',
+  height: 0.7,
+  type: ['grass', 'poison'],
+};
+
+pokemonList.push({
+  name: 'Charmander',
+  height: 0.6,
+  type: ['fire'],
+});
+
+pokemonList = pokemonList.concat([{
+  name: 'Squirtle',
+  height: 0.5,
+  type: ['water'],
+}]);
