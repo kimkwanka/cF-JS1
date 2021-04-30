ba20e858cec89be415424a744683b73d40c5126e
kimkwanka <kimkwanka@gmail.com>
2021-04-30 14:48:51 +0200

    refactor: rename attribute type > types

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ let pokemonList = [];
 pokemonList[0] = {
   name: 'Bulbasaur',
   height: 0.7,
-  type: ['grass', 'poison'],
+  types: ['grass', 'poison'],
 };
 
 pokemonList.push({
   name: 'Charmander',
   height: 0.6,
-  type: ['fire'],
+  types: ['fire'],
 });
 
 pokemonList = pokemonList.concat([{
   name: 'Squirtle',
   height: 0.5,
-  type: ['water'],
+  types: ['water'],
 }]);
