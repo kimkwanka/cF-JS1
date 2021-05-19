27cc35221d21be628c48d2cd1f314319cef9b121
kimkwanka <kimkwanka@gmail.com>
2021-05-20 01:20:03 +0200

    feat: show additional data on modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+// Lookup table for type weakness and strength
+const TYPE_LUT = {
+  normal: { weak: ['fighting'], strong: ['ghost'] },
+  fire: { weak: ['ground', 'rock', 'water'], strong: ['bug', 'steel', 'fire', 'grass', 'ice', 'fairy'] },
+  water: { weak: ['grass', 'electric'], strong: ['steel', 'fire', 'water', 'ice'] },
+  grass: { weak: ['flying', 'poison', 'bug', 'fire', 'ice'], strong: ['ground', 'water', 'grass', 'electric'] },
+  electric: { weak: ['ground'], strong: ['flying', 'steel', 'electric'] },
+  ice: { weak: ['fighting', 'rock', 'steel', 'fire'], strong: ['ice'] },
+  fighting: { weak: ['flying', 'psychic', 'fairy'], strong: ['rock', 'bug', 'dark'] },
+  poison: { weak: ['ground', 'psychic'], strong: ['fighting', 'poison', 'bug', 'grass', 'fairy'] },
+  ground: { weak: ['water', 'grass', 'ice'], strong: ['poison', 'rock', 'electric'] },
+  flying: { weak: ['rock', 'electric', 'ice'], strong: ['fighting', 'bug', 'grass', 'ground'] },
+  psychic: { weak: ['bug', 'ghost', 'dark'], strong: ['fighting', 'psychic'] },
+  bug: { weak: ['flying', 'rock', 'fire'], strong: ['fighting', 'ground', 'grass'] },
+  rock: { weak: ['fighting', 'ground', 'steel', 'water', 'grass'], strong: ['normal', 'flying', 'poison', 'fire'] },
+  ghost: { weak: ['ghost', 'dark'], strong: ['poison', 'bug', 'normal', 'fighting'] },
+  dark: { weak: ['fighting', 'bug', 'fairy'], strong: ['ghost', 'dark', 'psychic'] },
+  dragon: { weak: ['ice', 'dragon', 'fairy'], strong: ['fire', 'water', 'grass', 'electric'] },
+  steel: { weak: ['fighting', 'ground', 'fire'], strong: ['normal', 'flying', 'rock', 'bug', 'steel', 'grass', 'psychic', 'ice', 'dragon', 'fairy', 'poison'] },
+  fairy: { weak: ['poison', 'steel'], strong: ['fighting', 'bug', 'dark', 'dragon'] },
+};
+
 // eslint-disable-next-line func-names
 const pokemonRepository = (function () {
   const pokemonList = [];
@@ -55,6 +78,8 @@ const pokemonRepository = (function () {
       .then((res) => res.json())
       .then((data) => {
         pokemon.flavorText = data.flavor_text_entries[0].flavor_text;
+        // Filter out linebreaks and other weird characters that the API spits out
+        pokemon.flavorText = pokemon.flavorText.replace(/[^a-zA-Z é.!?,]/g, ' ');
       })
       .catch((e) => {
         console.error(e);
@@ -68,6 +93,19 @@ const pokemonRepository = (function () {
     const modalName = document.querySelector('.modal-name');
     modalName.innerText = pokemon.name;
 
+    const modalTypes = document.querySelector('.modal-types');
+    // Remove all children by replacing them with a single text node.
+    // textContent has much better performance than innerHTML because
+    // its value is not parsed as HTML.
+    // (check https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent)
+    modalTypes.textContent = null;
+    pokemon.types.forEach((t) => {
+      const typeTag = document.createElement('p');
+      typeTag.classList.add('modal-tag', t.type.name);
+      typeTag.innerText = t.type.name;
+      modalTypes.appendChild(typeTag);
+    });
+
     const mainType = pokemon.types[0].type.name;
 
     const modalImage = document.querySelector('.modal-img');
@@ -75,8 +113,41 @@ const pokemonRepository = (function () {
     modalImage.alt = pokemon.name;
     modalImage.className = `modal-img gradient--${mainType}`;
 
-    const modalText = document.querySelector('.modal-biometrics');
-    modalText.innerText = `Height: ${pokemon.height}\nWeight: ${pokemon.weight}`;
+    const modalHeight = document.querySelector('#modal-height');
+    modalHeight.innerText = `${pokemon.height * 10}cm`;
+
+    const modalWeight = document.querySelector('#modal-weight');
+    modalWeight.innerText = `${pokemon.weight / 10}kg`;
+
+    const modalFlavorText = document.querySelector('.modal-flavor-text');
+    modalFlavorText.innerText = pokemon.flavorText;
+
+    const modalWeaknesses = document.querySelector('.modal-weaknesses');
+    // Remove all children by replacing them with a single text node.
+    modalWeaknesses.textContent = null;
+
+    let weakAgainst = [];
+    let strongAgainst = [];
+
+    pokemon.types.forEach((type) => {
+      const typeName = type.type.name;
+      weakAgainst = weakAgainst.concat(TYPE_LUT[typeName].weak);
+      strongAgainst = strongAgainst.concat(TYPE_LUT[typeName].strong);
+    });
+
+    const filteredWeaknesses = weakAgainst.filter((weakness, index) => {
+      const isNotNullified = !strongAgainst.includes(weakness);
+      const isNoDuplicate = index === weakAgainst.indexOf(weakness);
+
+      return isNotNullified && isNoDuplicate;
+    });
+
+    filteredWeaknesses.forEach((weakness) => {
+      const weaknessTag = document.createElement('p');
+      weaknessTag.classList.add('modal-tag', weakness);
+      weaknessTag.innerText = weakness;
+      modalWeaknesses.appendChild(weaknessTag);
+    });
   }
 
   function showModal() {
